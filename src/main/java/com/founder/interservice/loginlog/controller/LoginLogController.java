a80a52e99b47ddd1e12e80252e487a611bf87c10
luoqihao <luo_qihao@FOUNDER.COM.CN>
2018-10-29 14:26:56 +0800

    添加登陆日志

--- src/main/java/com/founder/interservice/loginlog/controller/LoginLogController.java
+++ src/main/java/com/founder/interservice/loginlog/controller/LoginLogController.java
@@ -1,4 +1,36 @@
 package com.founder.interservice.loginlog.controller;
 
+import com.founder.interservice.loginlog.model.LoginLog;
+import com.founder.interservice.loginlog.service.LoginLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 记录登录用户信息
+ * @ClassName： LoginLogController,
+ * @Auther： 徐世洪
+ * @Description: 日志记录controller
+ * @CreateDate： 2018-10-24
+ * @Version: 1.0
+ */
+@Controller
 public class LoginLogController {
+    @Autowired
+    LoginLogService loginLogService;
+
+    /**
+     * 登录日志记录
+     * @param loginLog
+     * @return
+     */
+    @RequestMapping(value ="/saveLoginLog",method = {RequestMethod.GET,RequestMethod.POST})
+    @ResponseBody
+    public String saveLoginLog(LoginLog loginLog){
+        String result = loginLogService.saveLoginLog(loginLog);
+        return result;
+    }
+
 }
--- src/main/java/com/founder/interservice/loginlog/model/LoginLog.java
+++ src/main/java/com/founder/interservice/loginlog/model/LoginLog.java
@@ -1,4 +1,26 @@
 package com.founder.interservice.loginlog.model;
 
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@DynamicUpdate
+@Data
+@Table(name = "TB_ST_LOGINLOG")
 public class LoginLog {
+    @Id
+    private String xxzjbh;//信息主键编号
+    private String cxrJh;//查询人警号
+    private String cxrSfzh;//查询人身份证号
+    private String cxrXm;//查询人姓名
+    private String cxrLxdh;//查询人联系电话
+    @Column(insertable = false)
+    private Date cxrDlsj;//登陆时间
+
 }
