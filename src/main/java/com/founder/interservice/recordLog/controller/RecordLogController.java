6b019368e25fe4b2daf01be6f42e17de7376cf63
luoqihao <luo_qihao@FOUNDER.COM.CN>
2018-10-30 16:08:14 +0800

    新增“查询日志记录（RecordLog）”功能

--- src/main/java/com/founder/interservice/recordLog/controller/RecordLogController.java
+++ src/main/java/com/founder/interservice/recordLog/controller/RecordLogController.java
@@ -2,16 +2,25 @@ package com.founder.interservice.recordLog.controller;
 
 import com.founder.interservice.VO.ResultVO;
 import com.founder.interservice.enums.ResultEnum;
+import com.founder.interservice.recordLog.VO.RecordLogVo;
 import com.founder.interservice.recordLog.model.Querylog;
+import com.founder.interservice.recordLog.queryModel.QuerylogFilter;
 import com.founder.interservice.recordLog.service.RecordLogService;
+import com.founder.interservice.util.EasyUIPage;
 import com.founder.interservice.util.ResultVOUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName： RecordLogController
@@ -40,4 +49,52 @@ public class RecordLogController {
 
         return resultVO;
     }
+
+
+    @RequestMapping(value = "/queryQueryLogList",method = {RequestMethod.GET,RequestMethod.POST})
+    @ResponseBody
+    public Map<String,Object> queryQueryLog(QuerylogFilter filter,
+                                            @RequestParam(value = "page",defaultValue = "0") int page,
+                                            @RequestParam(value = "rows",defaultValue = "0") int rows){
+        Map<String,Object> objectMap = new HashMap<String, Object>();
+        List<RecordLogVo> recordLogVos = null;
+        try{
+            EasyUIPage easyUIPage = new EasyUIPage();
+            easyUIPage.setPage(page);
+            easyUIPage.setPagePara(rows);
+            int begin = easyUIPage.getBegin();
+            int end = easyUIPage.getEnd();
+
+            filter.setKssj(filter.getKssj());
+            filter.setJssj(filter.getJssj());
+            filter.setBegin(begin);
+            filter.setEnd(end);
+            Map<String, Object> resultMap = recordLogService.queryQueryLogList(filter);
+            int total = (int) resultMap.get("total");
+            List<Querylog> queryLogList = (List<Querylog>) resultMap.get("queryLogList");
+            if (queryLogList != null && !queryLogList.isEmpty()) {
+                recordLogVos = new ArrayList<>();
+                for (Querylog queryLog : queryLogList) {
+                    RecordLogVo recordLogVo = new RecordLogVo();
+                    BeanUtils.copyProperties(queryLog, recordLogVo);
+                    recordLogVos.add(recordLogVo);
+                }
+            objectMap.put("total", total);
+            objectMap.put("rows", recordLogVos);
+            }else {
+                objectMap.put("total",0);
+                objectMap.put("rows",new ArrayList<>());
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return objectMap;
+    }
+
+    @RequestMapping(value = "/recordLogView",method = {RequestMethod.GET,RequestMethod.POST})
+    public ModelAndView recordLogView(){
+        return new ModelAndView("RecordLog/RecordLog");
+    }
+
 }
---
+++ src/main/java/com/founder/interservice/recordLog/queryModel/QuerylogFilter.java
@@ -0,0 +1,32 @@
+package com.founder.interservice.recordLog.queryModel;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+
+
+@Data
+public class QuerylogFilter {
+
+
+    private String cxrSfzh;//查询人身份证号
+    private String cxrXm;//查询人姓名
+    private String cxrJh;//查询警号
+    private String cxrLxdh;//查询人联系电话
+    private String cxbsh;//查询标识号
+    private String bshlxdm;//标识号类型代码
+    private String bshlxmc;//标识号类型名称
+    private String asjbh;//案事件编号
+
+    private String kssj;
+    private String jssj;
+    private int begin;
+    private int end;
+
+}
